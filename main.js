0dd56b37977bab62ac5f394cc72ec6a7b01d8e27
grandfantasiapig <grandfantasiapig@github.com>
2020-01-29 23:43:45 +0800

    permission test 2

--- main.js
+++ main.js
@@ -1,21 +1,6 @@
 (function($) {
 	var url = 'https://script.google.com/macros/s/AKfycbwQYIUEoaRFy9Lnm7KcOpQnmwuJpdowKRTPaJ6FWPAqZJChSJgJ/exec';
 	
-	// window.jsonpCallback = function(res) {
-	// 	alert(res);
-	// 	alert($('#s321')[0].test);
-	// };
-	
-	/* void jsonp({
-		string url,
-		string arg,
-		string callback,
-		function success(res),
-		timeout = {
-			int time, //unit min second
-			function reponese	
-		}
-	}) */
 	$.jsonp({
 		url: 'https://script.google.com/macros/s/AKfycbwQYIUEoaRFy9Lnm7KcOpQnmwuJpdowKRTPaJ6FWPAqZJChSJgJ/exec',
 		arg: 'mode=requestPermissions&callback=jsonpCallback',
